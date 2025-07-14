5ba8cd1d19c6d8524eaea7cde776fb198cba68f6
kush-lhmm <kush@lhmm.in>
2025-07-14 16:02:43 +0530

    GTM Back!

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -119,13 +119,13 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         {/* Google Tag Manager */}
-        {/* <script dangerouslySetInnerHTML={{
+        <script dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PKBNXSM5');`
-        }} /> */}
+        }} />
         {/* End Google Tag Manager */}
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
@@ -185,14 +185,14 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         className={`${poppins.variable} ${DavidLibre.variable} antialiased`}
       >
         {/* Google Tag Manager (noscript) */}
-        {/* <noscript>
+        <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-PKBNXSM5"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           />
-        </noscript> */}
+        </noscript>
         {/* End Google Tag Manager (noscript) */}
         <AnnouncementCarousel />
         <Header />
